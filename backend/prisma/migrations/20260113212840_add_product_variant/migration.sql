a525db69c1c5dc92cf9674736449255a593a1cba
Connor Streng <cstreng95@gmail.com>
2026-01-13 16:30:53 -0500

    feat: add product variants to db

---
+++ backend/prisma/migrations/20260113212840_add_product_variant/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE `Variant` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `productId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Variant` ADD CONSTRAINT `Variant_productId_fkey` FOREIGN KEY (`productId`) REFERENCES `Product`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
