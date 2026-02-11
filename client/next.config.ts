7ddcc4e64f955c39f33a4d3aafe1440565b77bfa
Connor Streng <cstreng95@gmail.com>
2026-02-11 16:45:29 -0500

    feat: start using scss

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // should catch Next Links with non existent hrefs during dev/build
   typedRoutes: true,
+  sassOptions: {
+    additionalData: "@use '@/styles/variables' as *;",
+  },
 };
 
 export default nextConfig;
