85b7ea440ae0c9f4bb04b6c9b2cb1dc83bd624d0
Connor Streng <cstreng95@gmail.com>
2026-02-16 15:48:11 -0500

    feat: tweak grid

--- client/src/components/atoms/Grid/Grid.tsx
+++ client/src/components/atoms/Grid/Grid.tsx
@@ -1,9 +1,11 @@
-import { PropsWithChildren } from "react";
+import { HTMLElementType, PropsWithChildren } from "react";
 import styles from "./styles.module.scss";
 
-// TODO: 'as' prop that determines tag used?
-type GridProps = PropsWithChildren<{}>;
+type GridProps = PropsWithChildren<{
+  as?: HTMLElementType;
+}>;
 
-export default function Grid({ children }: GridProps) {
-  return <div className={styles.grid}>{children}</div>;
+export default function Grid({ as = "div", children }: GridProps) {
+  const Tag = as;
+  return <Tag className={styles.grid}>{children}</Tag>;
 }
